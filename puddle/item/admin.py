fdad39587dd9b819116bdcec2987c4f179d7f223
Charles <adeakinyemi129@gmail.com>
2024-11-11 18:30:52 +0100

    migrated item model

--- puddle/item/admin.py
+++ puddle/item/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category
+from .models import Category, Item
 
 admin.site.register(Category)     #registering the model to the admin
+admin.site.register(Item) #registering the model to the admin)
