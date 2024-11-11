9c1d2b3c23f0d3d17353f1349be47c02e67e7279
Charles <adeakinyemi129@gmail.com>
2024-11-11 14:36:03 +0100

    added web views

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
+
+def index(request):
+
+    return render(request, 'core/index.html')  #second param is the template
+
+def contact(request):
+    return render(request, 'core/contact.html')
