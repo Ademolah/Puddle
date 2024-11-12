781911238150b6b71780ebc82a0a859a7a068f40
Charles <adeakinyemi129@gmail.com>
2024-11-12 10:29:52 +0100

    added related and product details

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -22,7 +22,7 @@ from core.views import index, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('core.urls')),
+    path('items/', include('item.urls')),
     path('', index, name='index'),
     path('contact/', contact, name='contact')
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
