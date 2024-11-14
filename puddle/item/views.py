7274f7b486ea099d12745a533217aec0b21ddd4d
Charles <adeakinyemi129@gmail.com>
2024-11-14 08:47:01 +0100

    added search button

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -7,6 +7,15 @@ from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 # Create your views here.
+
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+
+    return render(request, 'item/items.html', {
+        'items': items,
+    })
+
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
