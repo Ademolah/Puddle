75befdd6dee907e248c1f24c795d69bc048003d3
Charles <adeakinyemi129@gmail.com>
2024-11-17 22:12:15 +0100

    added stripe

--- puddle/checkout/views.py
+++ puddle/checkout/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.views import View
+from django.http import JsonResponse
+from .models import Order, Payment, WebhookEvent
+import stripe
+
 
 
 # Create your views here.
